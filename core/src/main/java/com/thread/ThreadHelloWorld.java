fb6fd55777605c653b950e5e3ea0e19edc73888d
manjitwaikhom <manjitwaikhom@gmail.com>
2016-12-06 19:04:54 +0530

    thread by implementing runnable

--- core/src/main/java/com/thread/ThreadHelloWorld.java
+++ core/src/main/java/com/thread/ThreadHelloWorld.java
@@ -1,21 +1,21 @@
 package com.thread;
+
 /*----------------------THREADING------------------------------*/
-class ThreadHelloWorld{
-public static void main(String as[]){
-System.out.println("main begin");
-Thread t=Thread.currentThread();
-System.out.println(t);
-System.out.println(t.getName());
-System.out.println(t.getPriority());
-t.setName("waikhom_thread");
-t.setPriority(9);
-System.out.println(t);
-System.out.println(t.getName());
-System.out.println(t.getPriority());
+class ThreadHelloWorld {
+	public static void main(String as[]) {
+		System.out.println("main begin");
+		Thread t = Thread.currentThread();
+		System.out.println(t);
+		System.out.println(t.getName());
+		System.out.println(t.getPriority());
+		t.setName("waikhom_thread");
+		t.setPriority(9);
+		System.out.println(t);
+		System.out.println(t.getName());
+		System.out.println(t.getPriority());
 
-int x=10/0;
+		int x = 10 / 0;
 
-System.out.println("main end");
+		System.out.println("main end");
+	}
 }
-}
-
