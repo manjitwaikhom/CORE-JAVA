fbb9f2f9306e7ab0782164f1ae347bda3e516091
manjitwaikhom <manjitsinghwk@gmail.com>
2019-03-25 10:23:50 +0530

    String Constant pool comments update

--- src/main/java/com/string/StringPool.java
+++ src/main/java/com/string/StringPool.java
@@ -1,6 +1,10 @@
 package com.string;
 
 /*-----STRING(string pool)-------*/
+/*Whenever we create a String Object, two objects will be created i.e. One in the Heap Area and
+One in the String constant pool and the String object reference always points to heap area object.
+When String s2=new String("manjit") execute, it will create two objects and pointing to the heap 
+area created object.*/
 public class StringPool{
 public static void main(String as[]){
 String s1="manjit";
@@ -8,9 +12,8 @@ String s2=new String("manjit");
 String s3=s1.intern();
 
 
-System.out.print("s1==s2....." );
-System.out.println(s1==s2);
-System.out.println("s1.equals(s2)...."+s1.equals(s2));
+System.out.println("s1==s2: " +s1==s2);
+System.out.println("s1.equals(s2):"+s1.equals(s2));
 
 }
 }
