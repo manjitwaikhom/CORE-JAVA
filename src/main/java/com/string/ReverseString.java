87078121f7f9679540bdab5e5a5b521e4c1fe999
manjitwaikhom <manjitsinghwk@gmail.com>
2019-03-28 10:46:45 +0530

    Reverse a string without using reverse() method

---
+++ src/main/java/com/string/ReverseString.java
@@ -0,0 +1,24 @@
+package com.string;
+
+import java.util.Scanner;
+//reverse a string without using reverse() method
+public class ReverseString {
+	 public static void main(String[] args)
+	    {
+	        System.out.println("Enter string to reverse:");
+	        
+	        Scanner read = new Scanner(System.in);
+	        String str = read.nextLine();
+	        String reverse = "";
+	        
+	        
+	        for(int i = str.length() - 1; i >= 0; i--)
+	        {
+	            reverse = reverse + str.charAt(i);
+	        }
+	        
+	        System.out.println("Reversed string is:");
+	        System.out.println(reverse);
+	        read.close();
+	    }
+}
