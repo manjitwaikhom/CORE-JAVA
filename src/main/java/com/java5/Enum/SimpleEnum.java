f78cb1cf03d5858a0b9ccd5a3fa3c319c031eb57
manjitwaikhom <manjitsinghwk@gmail.com>
2019-04-05 11:53:27 +0530

    Simple Enum

---
+++ src/main/java/com/java5/Enum/SimpleEnum.java
@@ -0,0 +1,15 @@
+package com.java5.Enum;
+
+enum Color 
+{ 
+    RED, GREEN, BLUE; 
+} 
+  
+public class SimpleEnum 
+{ 
+    public static void main(String[] args) 
+    { 
+        Color c1 = Color.RED; 
+        System.out.println(c1); 
+    } 
+}
