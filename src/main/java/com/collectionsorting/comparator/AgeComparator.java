93e060314ff83d5c7eef9a4f1563303c2058d60d
manjitwaikhom <manjitsinghwk@gmail.com>
2019-05-21 12:27:37 +0530

    Adding @Override

--- src/main/java/com/collectionsorting/comparator/AgeComparator.java
+++ src/main/java/com/collectionsorting/comparator/AgeComparator.java
@@ -3,7 +3,7 @@ package com.collectionsorting.comparator;
 import java.util.Comparator;
 
 public class AgeComparator implements Comparator<Student>{
-	
+	@Override
 	public int compare(Student s1,Student s2){  
 		if(s1.age==s2.age)  
 		return 0;  
--- src/main/java/com/collectionsorting/comparator/NameComparator.java
+++ src/main/java/com/collectionsorting/comparator/NameComparator.java
@@ -2,7 +2,8 @@ package com.collectionsorting.comparator;
 
 import java.util.Comparator;
 
-public class NameComparator implements Comparator<Student>{  
+public class NameComparator implements Comparator<Student>{
+	@Override
 	public int compare(Student s1,Student s2){  
 		return s1.name.compareTo(s2.name);  
 		}  
