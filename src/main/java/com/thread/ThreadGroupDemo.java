5c6e1ced46b7f5138ff18e2507dbb49c2641580b
manjitwaikhom <manjitsinghwk@gmail.com>
2019-06-12 15:47:22 +0530

    ThreadGroup

---
+++ src/main/java/com/thread/ThreadGroupDemo.java
@@ -0,0 +1,46 @@
+package com.thread;
+
+class NewThread extends Thread 
+{ 
+	NewThread(String threadname, ThreadGroup tgob) 
+	{ 
+		super(tgob, threadname); 
+		start(); 
+	} 
+public void run() 
+	{ 
+
+		for (int i = 0; i < 1000; i++) 
+		{ 
+			try
+			{ 
+				Thread.sleep(110); 
+			} 
+			catch (InterruptedException ex) 
+			{ 
+				System.out.println("Exception encounterted"); 
+			} 
+		} 
+	} 
+} 
+public class ThreadGroupDemo 
+{ 
+	public static void main(String arg[]) 
+	{ 
+		// creating the thread group 
+		ThreadGroup tg = new ThreadGroup("parent thread group"); 
+
+		NewThread t1 = new NewThread("one", tg); 
+		System.out.println("Starting one"); 
+		NewThread t2 = new NewThread("two", tg); 
+		System.out.println("Starting two");
+		NewThread t3 = new NewThread("two", tg); 
+		System.out.println("Starting two"); 
+
+
+		// checking the number of active thread 
+		System.out.println("number of active thread: "
+						+ tg.activeCount()); 
+	} 
+} 
+
