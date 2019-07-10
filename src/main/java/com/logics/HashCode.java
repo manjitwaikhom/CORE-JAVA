af54c4b17b38c987fe26145ff8a217f199788ec6
manjitwaikhom <manjitsinghwk@gmail.com>
2019-07-10 11:57:12 +0530

    HashCode

---
+++ src/main/java/com/logics/HashCode.java
@@ -0,0 +1,27 @@
+package com.logics;
+
+public class HashCode {
+
+    public static void main(String[] args) {
+	System.out.println(new Book("richest man in babylon").hashCode());
+	System.out.println(new Book("richest man in babylon").hashCode());
+	System.out.println(new Book("richest man in babylon").hashCode());
+    }
+
+}
+
+class Book{
+    String name;
+
+    public Book(String name) {
+	super();
+	this.name = name;
+    }
+    
+    /*@Override
+    public int hashCode() {
+    return 1234;
+    }*/
+    
+    
+}
