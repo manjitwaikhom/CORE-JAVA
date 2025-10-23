3e30c67d5bc97b71b97c4b26036124f92d366242
manjitwaikhom <manjitsinghwk@gmail.com>
2025-10-23 14:42:58 +0530

    Count Repeated Numbers or Elements

--- src/main/java/com/arrays/RepeatedElements.java
+++ src/main/java/com/arrays/RepeatedElements.java
@@ -1,6 +1,9 @@
 package com.arrays;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 class RepeatedElements{
 
@@ -17,9 +20,20 @@ for(int i = 0; i < numbers.length-1; i++) {
 		System.out.println(numbers[i]+" occurs "+count+" times.");
 
 }
-	
-    
 }
+
+	Map<Integer, Integer> countMapTest = new LinkedHashMap<>();
+	countMapTest.put(1,3);
+	countMapTest.put(2,2);
+	countMapTest.put(1,1);
+
+// using java streams
+	Map<Integer, Integer> countMap = new HashMap<>();
+	for (int num : numbers) {
+		countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+	}
+
+	//System.out.println(countMap);
 
 }
 
