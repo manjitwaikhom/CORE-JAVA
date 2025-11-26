d9a6bc0d78a9579b3b14c898fc3f23da3ef9a96d
manjitwaikhom <manjitsinghwk@gmail.com>
2025-11-26 11:44:28 +0530

    java 17 Sealed Class

---
+++ src/main/java/com/java17/MainAppJava17SealedClass.java
@@ -0,0 +1,32 @@
+package com.java17;
+
+sealed class Device permits Mobile,Tab{
+
+}
+// subclass of a sealed class should be either final or non-sealed
+non-sealed class Mobile extends Device{
+
+}
+// subclass of a sealed class should be either final or non-sealed
+final class Tab extends Device{
+
+}
+
+sealed interface  Vehicle permits FourWheeler, TwoWheeler{
+
+}
+
+non-sealed class FourWheeler implements Vehicle{
+
+}
+
+non-sealed class TwoWheeler implements Vehicle{
+
+}
+
+
+public class MainAppJava17 {
+    public static void main(String[] args) {
+
+    }
+}
