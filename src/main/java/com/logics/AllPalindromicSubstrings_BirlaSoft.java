0e3ca63078b4d9c981b94122a0fa9a3ad990faff
manjitwaikhom <manjitsinghwk@gmail.com>
2026-01-28 13:39:42 +0530

    All palindromic substring Using Rabin Karp and Center Expansion

--- src/main/java/com/logics/AllPalindromicSubstrings_BirlaSoft.java
+++ src/main/java/com/logics/AllPalindromicSubstrings_BirlaSoft.java
@@ -1,32 +1,82 @@
 package com.logics;
 
+import com.sun.source.tree.Tree;
+
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class AllPalindromicSubstrings_BirlaSoft {
 
-    private static void expandAndAdd(String s, int left, int right, Set<String> set) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            set.add(s.substring(left, right + 1));
+
+    //Using Rabin Karp and Center Expansion
+
+    private static void expandAndAdd(String str, int left, int right, Set<String> set) {
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            set.add(str.substring(left, right + 1));
             left--;
             right++;
         }
     }
 
-    public static Set<String> allPalindromes(String s) {
+    public static Set<String> allPalindromes(String str) {
         Set<String> palindromes = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             // Odd length
-            expandAndAdd(s, i, i, palindromes);
+            expandAndAdd(str, i, i, palindromes);
             // Even length
-            expandAndAdd(s, i, i + 1, palindromes);
+            expandAndAdd(str, i, i + 1, palindromes);
         }
         return palindromes;
     }
 
+    public static Set<String> allPalindromeBrutForce(String s){
+        int n = s.length();
+
+        //usig TreeSet to mantain order
+        Set<String> result=new HashSet<>();
+
+        //to store substrings
+        String cur="";
+
+        for (int i = 0; i < n; i++) {
+
+            for(int j=i+1;j<=n;j++){
+                cur=s.substring(i,j);
+                // check if cur is palindrome
+                Boolean isPalin= isPalindrome(cur);
+                if(isPalin){
+                    result.add(cur);
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    public static Boolean isPalindrome(String str){
+        Boolean isPalindrome=false;
+        String reverse="";
+        if(str!=null && str !="") {
+
+            for (int k = str.length()-1; k >= 0; k--) {
+                reverse = reverse + str.charAt(k);
+            }
+            if (reverse.equals(str)) {
+                isPalindrome = true;
+            }
+        }
+
+        return isPalindrome;
+    }
+
     public static void main(String[] args) {
         String str = "babaab";
-        Set<String> result = allPalindromes(str);
-        System.out.println("All Palindromic Substrings: " + result);
+        //Set<String> result = allPalindromes(str);
+        Set<String> result = allPalindromeBrutForce(str);
+        System.out.println("All Palindromic Substrings: Expected[aa, a, aba, b, bab, baab]");
+        System.out.println( "Result: " + result);
+
     }
 }
