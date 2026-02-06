3345da9b6c46279aa03e3aab5d99548ff30fc852
manjitwaikhom <manjitsinghwk@gmail.com>
2026-02-06 14:31:06 +0530

    Frequency Of a Character in a String

---
+++ src/main/java/com/stream/FrequencyOfACharacter.java
@@ -0,0 +1,28 @@
+package com.stream;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FrequencyOfACharacter {
+    public static void main(String[] args) {
+        String str="manjitmanja";
+
+        //using streams
+        Map<Character,Long> freq=str.chars().mapToObj(c->(char) c ).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        System.out.println("Using Streams: "+freq);
+
+
+
+        //using loop
+        Map<Character,Long> freq2=new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            if(freq2.containsKey(str.charAt(i))){
+                freq2.put(str.charAt(i),freq2.get(str.charAt(i))+1);
+            }else{
+                freq2.put(str.charAt(i),1L);
+        }}
+        System.out.println("Using Loop:    "+freq2);
+    }
+}
